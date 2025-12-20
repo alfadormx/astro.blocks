b58003a7a341e5ef043a459eaf719a2ba2c8f4ce
alfadormx <alfadormx@users.noreply.github.com>
2025-12-20 22:03:30 +0100

    chore: added tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@
 import { defineConfig } from 'astro/config';
 import site_config from './vendor/integration';
 
+import tailwindcss from '@tailwindcss/vite';
+
 const siteConfig = { config: './src/config.yaml' };
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [site_config(siteConfig)],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
