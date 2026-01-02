7ea31d2663f55a07900ef9e16ba65ef8be95be9a
alfadormx <alfadormx@users.noreply.github.com>
2026-01-02 02:06:45 +0100

    feat(FAQ): added new block

---
+++ src/types/faq.types.ts
@@ -0,0 +1,10 @@
+import type { ContainerProps } from './container.types';
+import type { HeadlineProps } from './headline.types';
+import type { ItemsGridProps } from './itemsgrid.types';
+
+export interface FAQProps {
+  container?: Partial<ContainerProps>;
+  headline?: Partial<HeadlineProps>;
+  itemsGrid?: Partial<ItemsGridProps>;
+  items?: ItemsGridProps['items'];
+}
