cbd0fde545ff96100ee351f3e3edcc1532de4d11
alfadormx <alfadormx@users.noreply.github.com>
2026-01-02 16:43:04 +0100

    feat(Content): added new block

---
+++ src/types/content.types.ts
@@ -0,0 +1,16 @@
+import type { ContainerProps } from './container.types';
+import type { HeadlineProps } from './headline.types';
+import type { ButtonProps } from './button.types';
+import type { ItemsGridProps } from './itemsgrid.types';
+import type { ImageProps } from '~/utils/images-optimization';
+
+export interface ContentProps {
+  container?: Partial<ContainerProps>;
+  headline?: Partial<HeadlineProps>;
+  image?: ImageProps;
+  content?: string;
+  action?: ButtonProps;
+  items?: ItemsGridProps['items'];
+  imageOnLeft?: boolean;
+  reverseOnMobile?: boolean;
+}
