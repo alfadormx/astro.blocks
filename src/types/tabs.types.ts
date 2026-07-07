5dd255c45f2d00840b3d42363b5eaf08eb235c0a
Hector Hernandez <h.hernandezrodriguez@coolblue.nl>
2026-07-07 14:04:28 +0200

    feat(tabs): add tabList style config to Tabs

--- src/types/tabs.types.ts
+++ src/types/tabs.types.ts
@@ -1,5 +1,8 @@
 import type { ButtonProps } from '~/types/button.types';
 import type { ContainerProps } from '~/types/container.types';
+import type { SimpleBackgroundProps } from '~/types/background.types';
+import type { SpacingConfig } from '~/types/spacing.types';
+import type { BorderConfig } from '~/types/border.types';
 
 export interface TabsProps {
   items: Partial<ButtonProps>[];
@@ -7,4 +10,10 @@ export interface TabsProps {
   activeTabConfig?: Partial<ButtonProps>;
   orientation?: 'horizontal' | 'vertical';
   container?: Partial<ContainerProps>;
+  tabList?: {
+    background?: SimpleBackgroundProps;
+    spacing?: SpacingConfig;
+    border?: BorderConfig;
+    class?: string;
+  };
 }
